016e10cf6ce7ced492d4adc1ef952c55aff217c6
Sahil Tamboli <tambolisahil36@gmail.com>
2025-04-02 19:55:01 +0530

    blog test ui added

--- resources/js/components/ui/footer.tsx
+++ resources/js/components/ui/footer.tsx
@@ -77,7 +77,7 @@ interface MenuItem {
     ],
   }: Footer2Props) => {
     return (
-      <section className="py-32 px-20">
+      <section className="py-20 px-20">
         <div className="container">
           <footer>
             <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
