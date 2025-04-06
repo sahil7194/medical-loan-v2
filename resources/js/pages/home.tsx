810034d2c07444b2a1e3e3508206f2c7f42186d8
Sahil Tamboli <tambolisahil36@gmail.com>
2025-04-06 15:33:20 +0530

    layout setup stage one done

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,13 +1,18 @@
+import AppLayout from '@/layouts/app-layout'
 import React from 'react'
-
+import { Head } from '@inertiajs/react';
 const Home = () => {
-  return (
-    <div>
-        <h1>
-            This home page of new loan application site
-        </h1>
-    </div>
-  )
+    return (
+        <AppLayout>
+            <Head title="Dashboard" />
+            <div>
+                <h1>
+                    This home page of new loan application site
+                </h1>
+            </div>
+        </AppLayout>
+
+    )
 }
 
 export default Home
